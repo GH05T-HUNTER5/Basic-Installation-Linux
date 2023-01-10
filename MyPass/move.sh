1288eb0558e108c0664aa711ea5467f64e781ce8
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2023-01-10 14:08:47 +0530

    Update move.sh

--- MyPass/move.sh
+++ MyPass/move.sh
@@ -3,6 +3,34 @@ white="$(printf '\033[37m')"
 colorz="$(printf '\033[36m')"
 red="$(printf '\033[31m')"
 clear
+main_package() {
+	if [[ $(command -v wget) && $(command -v nmap) && $(command -v tor) && $(command -v python) && $(command -v python2) && $(command -v curl) && $(command -v clang) && $(command -v dnsutils) && $(command -v php) ]]; then
+		echo -e "Installed."
+	else
+		pkgs=(wget nmap tor python python2 curl clang dnsutils php)
+		for pkg in "${pkgs[@]}"; do
+			type -p "$pkg" &>/dev/null || {
+				echo -e "Installing package"
+				if [[ $(command -v pkg) ]]; then
+					pkg install "$pkg" -y
+				elif [[ $(command -v apt) ]]; then
+					sudo apt install "$pkg" -y
+				elif [[ $(command -v apt-get) ]]; then
+					sudo apt-get install "$pkg" -y
+				elif [[ $(command -v pacman) ]]; then
+					sudo pacman -S "$pkg" --noconfirm
+				elif [[ $(command -v dnf) ]]; then
+					sudo dnf -y install "$pkg"
+				elif [[ $(command -v yum) ]]; then
+					sudo yum -y install "$pkg"
+				else
+					echo -e "Install packages manually."
+					{ reset_color; exit 1; }
+				fi
+			}
+		done
+	fi
+}
 banners() {
 	clear
 	echo -e "${white} +-------------------------------------------------+"
@@ -30,6 +58,8 @@ basic_installation() {
 	clear
 	banners
 	echo " [++] Please Wait..."
+        sleep 2
+        main_package
 	git clone https://github.com/cyweb/hammer >/dev/null 2>&1
 	git clone https://github.com/sqlmapproject/sqlmap >/dev/null 2>&1
 	git clone https://github.com/Und3rf10w/kali-anonsurf >/dev/null 2>&1
