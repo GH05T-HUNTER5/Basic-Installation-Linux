92512df0f25f71a3e0ae4257f9f6ce1c348d16ee
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2023-02-22 11:19:12 +0530

    Update engine.py

--- MyPass/engine.py
+++ MyPass/engine.py
@@ -2,4 +2,4 @@
 from googlesearch import search
 query = input ("Enter what you want : ")
 for i in search(query, num=1000, stop=1000, pause=2):
-    print ("Result : " + i + "\n")
+    print ("own-engine information : " + i + "\n")
