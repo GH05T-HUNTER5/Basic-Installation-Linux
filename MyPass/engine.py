1a36f678f5f51afb3d7cc297e539d06d4252d1ab
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2023-02-23 01:52:00 +0530

    Update engine.py

--- MyPass/engine.py
+++ MyPass/engine.py
@@ -6,3 +6,18 @@ from googlesearch import search
 query = input (GREEN + "Search for anything : ")
 for i in search(query, num=1000, stop=1000, pause=2):
     print (RED + "own-engine info " + CYAN + ": " + GREEN + i + "\n")
+
+"""
+	echo "######################################################"
+	echo "######################################################"
+	echo "##################### READ THIS ######################"
+	echo "######################################################"
+	echo "######################################################"
+	echo "## Dear GitHub user, please do not copy this script ##"
+	echo "######################################################"
+	echo "##  All copyright under the laws of all countries   ##"
+	echo "######################################################"
+	echo "##        Copyright of  GH05T HUNTER5, 2023         ##"
+	echo "######################################################"
+	echo "######################################################"
+"""
